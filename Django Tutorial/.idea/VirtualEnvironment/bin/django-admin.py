0957111b0923ad78793d12105a0d631abf562bff
IHaveNoClueI <xynatic@gmail.com>
2020-02-19 21:20:18 +0100

    Django Tutorial

---
+++ Django Tutorial/.idea/VirtualEnvironment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/x337/PycharmProjects/Django Tutorial/.idea/VirtualEnvironment/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
